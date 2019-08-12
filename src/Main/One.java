df72e5bfdb440d656c7bbf58bccff700e266f1ec
ABHISHEK <abhishek.gupta1_cs18@gla.ac.in>
2019-08-12 15:21:12 +0530

    table of any int

--- src/Main/One.java
+++ src/Main/One.java
@@ -1,8 +1,21 @@
 
 package Main;
 
+import java.util.Scanner;
 public class One {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        One ab = new One();
+        ab.input(n);
+
+    }
+
+    void input(int a) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(a + " * " + i + "=" + a * i);
+
+        }
     }
 }
