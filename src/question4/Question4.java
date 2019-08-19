4371f9a0b8d7bec812378f4bf2a3af0c68ffd828
ABHISHEK <abhishek.gupta1_cs18@gla.ac.in>
2019-08-19 15:10:16 +0530

    add Scanner object and i/o operations

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -1,7 +1,16 @@
 package question4;
 
+import java.util.Scanner;
 public class Question4 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a > 0)
+            System.out.println("positive");
+        else if (a < 0)
+            System.out.println("negative");
+        else if (a == 0)
+            System.out.println("zero");
 
     }
 }
