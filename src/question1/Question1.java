8bdcfb7846dd1a46fa1716c2f6b286fab752ca0f
ABHISHEK <abhishek.gupta1_cs18@gla.ac.in>
2019-08-19 14:18:51 +0530

    add method:main()

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 package question1;
 
 public class Question1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println("welcome" + name);
+    }
 }
