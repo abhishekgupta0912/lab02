50acf3624d20a7640153166c70f04263059cfafb
ABHISHEK <abhishek.gupta1_cs18@gla.ac.in>
2019-08-19 14:43:52 +0530

    add scanner obj and i/o operation

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -1,7 +1,13 @@
 package question2;
 
+import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1, s2;
+        s1 = sc.next();
+        s2 = sc.next();
+        System.out.println(s1 + " University " + s2);
 
     }
 }
